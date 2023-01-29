ec8a5c46bd38d5084e27d5db3df6d4e5fcf09541
hamza shadeau <shadeau99@gmail!.com>
2023-01-30 00:19:53 +0100

    add change every 24h

--- main.py
+++ main.py
@@ -14,15 +14,17 @@ if st.button('احصل على برنامجك اليومي'):
              "27/28", "29/30", "31/32", "33/34", "35/36", "37/38", "39/40", "41/42", "43/44", "45/46", "47/48", "49/50",
              "51/52", "53/54", "55/56", "57/58", "59/60"]
     mon_dictionnaire = np.load('coran_df.npy', allow_pickle='TRUE').item()
-    mon_new_dictionnaire = {"تاريخ": datetime.now().date()}
-    for i in Names:
-        index = ahzab.index(mon_dictionnaire[i])
-        if index == 28:
-            index = 0
-        else:
-            index = ahzab.index(mon_dictionnaire[i]) + 1
-        mon_new_dictionnaire[i] = ahzab[index]
+    if datetime.now().date()!= mon_dictionnaire['تاريخ']:
+        mon_new_dictionnaire = {"تاريخ": datetime.now().date()}
+        for i in Names:
+            index = ahzab.index(mon_dictionnaire[i])
+            if index == 28:
+                index = 0
+            else:
+                index = ahzab.index(mon_dictionnaire[i]) + 1
+            mon_new_dictionnaire[i] = ahzab[index]
 
-    mon_dictionnaire = mon_new_dictionnaire
-    np.save('coran_df.npy', mon_dictionnaire)
-    st.write(mon_dictionnaire)
+    np.save('coran_df.npy', mon_new_dictionnaire)
+    del mon_new_dictionnaire['تاريخ']
+    st.write("برنامج القراءت ليوم " + str(datetime.today()))
+    st.write(mon_new_dictionnaire)
